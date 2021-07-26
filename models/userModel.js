3dca59c3bec9a50076fdaaf88207f2af711e5329
SP-Park <75590347+SP-Park@users.noreply.github.com>
2021-07-26 21:08:28 +0900

    mailtrap testing - resetPassword

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');
+
 
 const userSchema = new mongoose.Schema ({
     name: {
@@ -37,7 +39,9 @@ const userSchema = new mongoose.Schema ({
             message: 'Password are not the same'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordRestToken: String,
+    passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function(next){
@@ -66,6 +70,21 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // False means NOT changed
     return false
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordRestToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+        console.log({resetToken}, this.passwordRestToken)
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken
 }
 
 const User = mongoose.model('User', userSchema)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,8 @@ const router = express.Router()
 router
     .post('/signup', authController.signup)
     .post('/login', authController.login)
+    .post('/forgotPassword', authController.forgotPassword)
+    .patch('/resetPassword/:token', authController.resetPassword)
 
 router
     .route('/')
