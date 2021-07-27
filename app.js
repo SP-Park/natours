808af243fc2418d83930174b2b03a1fdd3e4d244
SP-Park <75590347+SP-Park@users.noreply.github.com>
2021-07-27 20:33:49 +0900

    Security: XSS, mongo sanitize

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const fs = require('fs');
+const hpp = require('hpp');
 const xss = require('xss-clean');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
-
-
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -42,7 +40,19 @@ app.use(express.json({ limit: '10kb' }))
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize())
 // Data snitization against XSS
+app.use(xss())
 
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}))
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
